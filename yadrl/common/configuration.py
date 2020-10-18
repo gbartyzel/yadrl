9e659ea30516c60ed3a1c4b02d1cc3a77739f834
Souphis <gbartyzel@hotmail.com>
2020-10-18 21:09:26 +0200

    Created configuration class

--- yadrl/common/configuration.py
+++ yadrl/common/configuration.py
@@ -1,19 +1,23 @@
-from dataclasses import InitVar, dataclass, field
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Tuple, Union
 
 import yaml
+from dataclasses import InitVar, dataclass, field
 
+import yadrl.common.exploration_noise as noise
 import yadrl.common.normalizer as norm
+import yadrl.common.scheduler as sch
 from yadrl.common.memory import ReplayMemory
 from yadrl.networks.body_parameter import BodyParameters
+
+T_CONFIG = Dict[str, Any]
 
 
 @dataclass
 class Configuration:
     experiment_name: str = field(init=False)
     type: str = field(init=False)
-    common: Dict[str, Any] = field(init=False)
-    specific: Dict[str, Any] = field(init=False)
+    common: T_CONFIG = field(init=False)
+    specific: T_CONFIG = field(init=False)
     state_normalizer: Any = field(init=False)
     exploration_strategy: Any = field(init=False)
     memory: Any = field(init=False)
@@ -21,25 +25,54 @@ class Configuration:
     config_path: InitVar[str]
 
     def __post_init__(self, config_path: str):
-        data, self.experiment_name = self._load_config(config_path)
+        data, self.experiment_name = self.__load_config(config_path)
         self.type = data['type']
         self.common = data['common']
         self.specific = data['specific']
         self.memory = ReplayMemory(**data['memory'])
-        self.state_normalizer = self._parse_state_normalizer(data)
+        self.state_normalizer = self.__parse_state_normalizer(data)
+        self.exploration_strategy = self.__parse_exploration_strategy(data)
         self.body = BodyParameters(data['body'])
 
-    def _load_config(self, config_path: str) -> Tuple[Dict[str, Any], str]:
+    @staticmethod
+    def __load_config(config_path: str) -> Tuple[T_CONFIG, str]:
         with open(config_path, 'r') as config_file:
             data = yaml.safe_load(config_file)
         keys = list(data.keys())
         assert len(keys) == 1
         return data[keys[0]], keys[0]
 
-    def _parse_exploration_strategy(self, data):
-        pass
+    def __parse_exploration_strategy(self, data: T_CONFIG) -> Union[
+        sch.BaseScheduler, noise.GaussianNoise]:
+        action_type = data['action_type']
+        if action_type == 'discrete':
+            return self.__create_scheduler_policy(data)
+        elif action_type == 'continuous':
+            return self.__create_noise_policy(data)
+        else:
+            raise ValueError(
+                'Invalid action type! Should be discrete or continuous')
 
-    def _parse_state_normalizer(self, data) -> norm.DummyNormalizer:
+    @staticmethod
+    def __create_scheduler_policy(data: T_CONFIG) -> sch.BaseScheduler:
+        if data['type'] == 'linear':
+            return sch.LinearScheduler(**data['parameters'])
+        elif data['type'] == 'exponential':
+            return sch.ExponentialScheduler(**data['parameters'])
+        else:
+            ValueError('Invalid epsilon schedule type!')
+
+    @staticmethod
+    def __create_noise_policy(data: T_CONFIG) -> noise.GaussianNoise:
+        if data['type'] == 'gaussian':
+            return noise.GaussianNoise(**data['parameters'])
+        elif data['type'] == 'ou':
+            return noise.OUNoise(**data['parameters'])
+        else:
+            ValueError()
+
+    @staticmethod
+    def __parse_state_normalizer(data: T_CONFIG) -> norm.DummyNormalizer:
         norm_type = data['type']
         if norm_type == 'rms':
             return norm.RMSNormalizer(**data['parameters'])
@@ -49,8 +82,3 @@ class Configuration:
             return norm.ImageNormalizer()
         else:
             return norm.DummyNormalizer()
-
-
-if __name__ == '__main__':
-    params = Configuration(config_path='../../experiments/cartpole_dqn.yaml')
-    print(params.body)
